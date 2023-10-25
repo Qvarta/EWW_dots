ec6fe4ddc605fdfebd5bd38f4963ce085f1dc228
Qvarta <qvarta77@gmail.com>
2023-10-25 11:30:13 +0300

    add cmus gruvbox theme

---
+++ cmus/lib.pl
@@ -0,0 +1,18 @@
+/home/ko/Music/Alan Walker-Faded.mp3
+/home/ko/Music/David Guetta feat. Sia - She Wolf.mp3
+/home/ko/Music/David Guetta & Sia -  - 00 - Titanium.mp3
+/home/ko/Music/David Guetta & Sia - Flames.mp3
+/home/ko/Music/David Guetta - Let’s Love.mp3
+/home/ko/Music/Nazareth - Snakes ’n’ Ladders - 01 - We Are Animals.mp3
+/home/ko/Music/Queen - Музыка - 00 - The Show Must Go On.mp3
+/home/ko/Music/Sia -Snowman.mp3
+/home/ko/Music/Sia - This Is Acting - 05 - Unstoppable.mp3
+/home/ko/Music/Пушной -  - 01 - Solo noi.mp3
+/home/ko/Music/Би-2 - Лунапарк - 01 - Bowie.mp3
+/home/ko/Music/Би-2 - Музыка - 00 - Ля-Ля Тополя.mp3
+/home/ko/Music/Ундервуд - Бабл-гам - 02 - Ангелы и аэропланы.mp3
+/home/ko/Music/Ундервуд - Избранное - 01 - Молодые львы.mp3
+/home/ko/Music/Ундервуд - Избранное - 11 - Очень хочется в Советский Союз.mp3
+/home/ko/Music/Ундервуд - Избранное - 13 - Это судьба.mp3
+/home/ko/Music/Ундервуд - Скарлетт Йоханссон едет в Херсон.mp3
+/home/ko/Music/Ундервуд - Ракеты на Марс.mp3
